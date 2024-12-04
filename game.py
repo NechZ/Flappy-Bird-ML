9d631694a690e9cb119cc9aadd692bad36a41e3f
NechZ <s231872@student.dhbw-mannheim.de>
2024-12-04 15:49:06 +0100

    Feature: Added Scoring with score text renderer

--- game.py
+++ game.py
@@ -19,6 +19,11 @@ class GameObject(pygame.sprite.Sprite):
         self.y = y    
         self.rect.topleft = (x, y)
         
+    def move_rect(self, x: float, y: float) -> None:
+        self.x = x
+        self.y = y
+        self.rect.topleft = (x, y)
+        
     def collides_with(self, other: 'GameObject') -> bool:
         return self.rect.colliderect(other.rect)
         
@@ -27,10 +32,13 @@ class Player(GameObject):
     def __init__(self) -> None:
         super().__init__(x_init=300, y_init=200, width=20, height=20, color=(255, 255, 255))
         
-        self.gravity = 9.81
-        self.acceleration = self.gravity
-        self.velocity = 0
-        self.jumpforce = -5 
+        self.gravity: float = 20
+        self.acceleration: float = self.gravity
+        self.velocity: float = 0
+        self.jumpforce: float  = -7.5
+        
+        self.score: int = 0
+        self.justScored: bool = False
         
     def draw_player(self, screen: pygame.Surface) -> None:
         self.draw_object(screen, self.x, self.y)    
@@ -56,67 +64,99 @@ class Player(GameObject):
                     return True
         
         return False
-        
+    
+    def check_score(self, obj: GameObject) -> bool:
+        collided: bool = self.collides_with(obj)
+        if collided and not self.justScored:
+            self.score += 1
+            self.justScored = True
+            return True
+        elif not collided:
+            self.justScored = False
+        return False
 
 class Gate():
     def __init__(self, speed: float = 1) -> None:        
         self.speed = speed
-        self.width = 150
-        self.height = 500
+        self.width: float = 150
+        self.height: float = 500
+         
+        self.gate_up: GameObject = GameObject(700, -self.height + 200, self.width, self.height, (0, 255, 0))
+        self.gate_down: GameObject = GameObject(700, 400, self.width, self.height, (0, 255, 0))
         
-        self.gate_up: GameObject = GameObject(500, -self.height + 200, self.width, self.height, (0, 255, 0))
-        self.gate_down: GameObject = GameObject(500, 400, self.width, self.height, (0, 255, 0))
-        
+        self.score_gate: GameObject = GameObject(700 + self.width/2, 0, self.width/10, self.height, (0, 0, 0))
         
     def draw_gate(self, screen: pygame.Surface) -> None:
         self.gate_up.draw_object(screen, self.gate_up.x, self.gate_up.y)
         self.gate_down.draw_object(screen, self.gate_down.x, self.gate_down.y)
+        self.score_gate.move_rect(self.score_gate.x, self.score_gate.y)
     
     def animate_gate(self, screen: pygame.Surface) -> None:        
         if (self.gate_up.x < -150 and self.gate_down.x < -150):
             x_up_new = 800 
             x_down_new = 800
+            x_score_new = 800
             
             y_random = random.randint(-200, 200)
             y_up_new = -self.height + 200 + y_random
             y_down_new = 400 + y_random
+            y_score_new = 0
             
         else:
             x_up_new = self.gate_up.x - 1 * self.speed
             x_down_new = self.gate_down.x -1 * self.speed
+            x_score_new = self.score_gate.x -1 * self.speed
             
             y_up_new = self.gate_up.y
             y_down_new = self.gate_down.y
+            y_score_new = self.score_gate.y
             
         self.gate_up.draw_object(screen, x_up_new, y_up_new)
         self.gate_down.draw_object(screen, x_down_new, y_down_new)
+        self.score_gate.move_rect(x_score_new, y_score_new)
         
     def get_gates(self) -> List[GameObject]:
         return [self.gate_up, self.gate_down]
         
+    def get_score_gate(self) -> GameObject:
+        return self.score_gate
         
 if __name__ == "__main__":
     pygame.init()
-    gate = Gate(speed=5)
-    player = Player()
-    
+    pygame.font.init()
     shutDownFlag = False
-    FPS = 60
-    screen = pygame.display.set_mode((800, 600))
-    gate.draw_gate(screen)
-    player.draw_player(screen)
-    clock = pygame.time.Clock()
-
+    
+    clock: pygame.time.Clock = pygame.time.Clock()
+    screen: pygame.Surface = pygame.display.set_mode((800, 600))
+    font: pygame.font.Font = pygame.font.SysFont("Arial", 30)
+    FPS: int = 60
+    
     while not shutDownFlag:
-        delta_time = clock.tick(FPS) / 1000
+        gate = Gate(speed=5)
+        player = Player()
+        gate.draw_gate(screen)
+        player.draw_player(screen)
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                shutDownFlag = True
-        screen.fill((0, 0, 0))
-        player.animate_player(screen, delta_time)
-        gate.animate_gate(screen)
-        if (player.is_dead(gate.get_gates())):
-            print("PLAYER DIED")
-            shutDownFlag = True
-        pygame.display.flip()
+        restartFlag = False
+        
+        while not restartFlag:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    shutDownFlag = True
+                    restartFlag = True
+            
+            delta_time = clock.tick(FPS) / 1000
+                    
+            screen.fill((0, 0, 0))
+            player.animate_player(screen, delta_time)
+            gate.animate_gate(screen)
+            text_surface = font.render(str(player.score), True, (255, 255, 255))
+            screen.blit(text_surface, (400, 20))
+            
+            player.check_score(gate.get_score_gate())    
+            restartFlag = player.is_dead(gate.get_gates())
+                        
+            pygame.display.flip()
+            
+        del gate
+        del player
